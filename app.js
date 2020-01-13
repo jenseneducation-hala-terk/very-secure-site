3bc18293ca31dde45c09c10a35466444c2986f1a
David Lundholm <david.lundholm@zocom.se>
2020-01-13 09:49:00 +0100

    Added scalability for more users

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const cookieParser = require('cookie-parser')
 // Express Instance
 const app = express()
 
+// Data
+// const user1 = {username:"admin", password:"password"}
+// const username1 = "admin"
+// const password1 = "password"
+// const username2 = "kalle"
+// const password2 = "grillkorv123"
+let users = [
+    {username:"admin", password:"password"}, 
+    {username:"kalle", password:"grillkorv123"}, 
+    {username:"olof", password:"asdf"}
+]
+
+let secrets = {
+    kalle: "Äpple",
+    olof: "Banan"
+}
+
 // Enable view rendering with EJS
 app.set("view engine", "ejs") 
 // Enable cookie header API
@@ -20,19 +37,30 @@ app.get('/', (req,res) => {
 
 // Handle authentication request
 app.post('/login', (req,res) => {
-    if(req.body.username == "admin" && req.body.password == "letmein"){
-        res.cookie("loggedIn", "true")
-        res.redirect('/secure')
-    }else{
+    let match = false
+    // for(let i = 0; i < users.length; i++){
+        // const user = users[i]
+    for(let user of users){
+        
+        if(req.body.username == user.username && req.body.password == user.password){
+            match = true
+            res.cookie("user", user.username)
+            res.redirect('/secure')
+            break;
+        }
+    }
+    if(match == false){
         res.status(403)
         res.send("Invalid Credentials")
+
     }
 })
 
 // Secured route
 app.get('/secure', (req,res) => {
-    if(req.cookies.loggedIn == "true"){
-        res.render("secure")
+    if(req.cookies.user){
+        const secret = secrets[ req.cookies.user ]
+        res.render("secure", {secret})
     }else{
         res.status(403)
         res.send("Unauthorized!")
