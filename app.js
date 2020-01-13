5f27f5771d7efb75e079da99de221c89825e764b
David Lundholm <david.lundholm@zocom.se>
2020-01-13 09:06:55 +0100

    Added comments

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
+// Packages
 const express = require('express')
 const cookieParser = require('cookie-parser')
 
+// Express Instance
 const app = express()
-app.set("view engine", "ejs")
-app.use(cookieParser())
-app.use(express.urlencoded())
 
+// Enable view rendering with EJS
+app.set("view engine", "ejs") 
+// Enable cookie header API
+app.use(cookieParser()) 
+// Enable request body parsing
+app.use(express.urlencoded()) 
+
+
+// Serve main page
 app.get('/', (req,res) => {
     res.render("index")
 })
 
+// Handle authentication request
 app.post('/login', (req,res) => {
     if(req.body.username == "admin" && req.body.password == "letmein"){
         res.cookie("loggedIn", "true")
         res.redirect('/secure')
     }else{
         res.status(403)
+        res.send("Invalid Credentials")
     }
 })
 
+// Secured route
 app.get('/secure', (req,res) => {
     if(req.cookies.loggedIn == "true"){
         res.render("secure")
     }else{
-        res.status(403).send("")
+        res.status(403)
+        res.send("Unauthorized!")
     }
 })
 
+// Start server
 app.listen(5000, () => console.log("We rollin"))
